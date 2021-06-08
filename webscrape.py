c0f413a1c48997e7c1b321b59b1728988bd35704
nconnel4 <nconnel4@hotmail.com>
2021-06-08 16:17:27 -0400

    Add browser and page load functions

---
+++ webscrape.py
@@ -0,0 +1,25 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as Soup
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def create_browser():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    return browser
+
+
+def open_page(browser, url):
+    browser.visit(url)
+
+
+def scrape_mars_data():
+    browser = create_browser()
+    open_page(browser, 'https://redplanetscience.com')
+
+
+
+if __name__ == '__main__':
+    scrape_mars_data()
