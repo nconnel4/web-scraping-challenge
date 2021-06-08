ad435935386197b65c2d589669b1be96d00df43b
nconnel4 <nconnel4@hotmail.com>
2021-06-08 16:42:22 -0400

    Add scrape_mars_news function
    Function scrapes the latest news headline and the paragraph text on the page

--- scrape_mars.py
+++ scrape_mars.py
@@ -3,6 +3,15 @@ from bs4 import BeautifulSoup as Soup
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
+import logging
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
 
 def create_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -13,11 +22,29 @@ def create_browser():
 
 def open_page(browser, url):
     browser.visit(url)
+    html = browser.html
+
+    return html
+
+
+def scrape_mars_news(html):
+    news_soup = Soup(html, 'html.parser')
+
+    element = news_soup.select_one('div.list_text')
+    news_title = element.find('div', class_='content_title').get_text()
+    logger.info(news_title)
+
+    news_paragraph = element.find('div', class_='article_teaser_body').get_text()
+    logger.info(news_paragraph)
+
+    return news_title, news_paragraph
 
 
 def scrape():
     browser = create_browser()
-    open_page(browser, 'https://redplanetscience.com')
+    html = open_page(browser, 'https://redplanetscience.com')
+
+    news_title, news_paragraph = scrape_mars_news(html)
 
 
 
